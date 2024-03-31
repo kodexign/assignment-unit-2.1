ebdeb9e8b5edf223f29706c4e0defc19dcb08de4
Kalia Xiong <kaliaxyooj@gmail.com>
2024-03-31 08:56:44 -0500

    variable 2 declared

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,9 +1,12 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'kalia';
+
 console.log (firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'xiong';
 
+console.log (lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
