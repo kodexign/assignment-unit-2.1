42656bcae78044842664ebc1bba8f8982b86318e
Kalia Xiong <kaliaxyooj@gmail.com>
2024-03-31 08:59:07 -0500

    variable 3 declared

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,14 +2,17 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'kalia';
 
-console.log (firstName);
+console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'xiong';
 
-console.log (lastName);
+console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
+let fullName = 'firstName' + 'lastName';
+
+console.log(fullName)
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
