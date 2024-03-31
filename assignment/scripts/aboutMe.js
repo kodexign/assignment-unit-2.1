fb97223ce605cf595a2ac1cb5e7d4b39ccb217e2
Kalia Xiong <kaliaxyooj@gmail.com>
2024-03-31 10:01:58 -0500

    added variable 10 and 11

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -53,16 +53,20 @@ console.log(pets);
 let friendsPets = 1;
 
 console.log(friendsPets);
-
 // 10 - Add two pets to your `pets` variable
+pets = pets + 2;
 
+console.log(pets);
 
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets= 2;
 
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
+let result = ;
+
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
